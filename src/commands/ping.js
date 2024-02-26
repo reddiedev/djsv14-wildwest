cacfa0fe66cc8dade433de1327d3a552711951a3
reddiedev <105657266+reddiedev@users.noreply.github.com>
2024-02-27 02:34:51 +0800

    updated ping cmd

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,8 +1,19 @@
-const { SlashCommandBuilder } = require(`discord.js`);
+const { SlashCommandBuilder, CommandInteraction } = require(`discord.js`);
 
 module.exports = {
     data: new SlashCommandBuilder().setName(`ping`).setDescription(`Replies with Pong!`),
+    /**
+     *
+     * @param {CommandInteraction} interaction
+     */
     async execute(interaction) {
-        await interaction.reply(`Pong!`);
-    },
+        await interaction.deferReply();
+        const reply = await interaction.fetchReply();
+        const clientPing = reply.createdTimestamp - interaction.createdTimestamp;
+        const websocketPing = interaction.client.ws.ping;
+
+        await interaction.editReply({
+            content: `Pong!\n**Client: **\`${clientPing}\` ms\n**Websockeet: **\`${websocketPing}\` ms`
+        });
+    }
 };
